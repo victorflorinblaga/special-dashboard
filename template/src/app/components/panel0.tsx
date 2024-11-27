b17a7095f1814fb73c0b75e84dcbd707c6c3bf41
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2024-11-27 12:21:15 +0100

    updating contents of template/src/app/components/panel0.tsx

--- template/src/app/components/panel0.tsx
+++ template/src/app/components/panel0.tsx
@@ -1,11 +1,35 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 
-function panel0() {
+const ServiceRatingsPage = () => {
   return (
-    <div>
-      <h1>Panel 0</h1>
+    <div className="w-full h-full flex flex-col gap-4 p-4">
+      <Card className="bg-blue-600 text-white p-5">
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold">Current Service Ratings</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <CardDescription className="mt-4 text-white">This quarter's data was analyzed and here are our findings</CardDescription>
+        </CardContent>
+      </Card>
+      <Separator className="bg-white h-px" />
+      <div className="flex flex-row gap-4 flex-1">
+        <div className="bg-blue-600 flex-1 p-4 text-white">
+          statistic 1 missing
+        </div>
+        <div className="bg-blue-600 flex-1 p-4 text-white">
+          statistic 2 missing
+        </div>
+        <div className="bg-blue-600 flex-1 p-4"></div>
+      </div>
     </div>
   );
-}
+};
 
-export default panel0;
-
+export default ServiceRatingsPage;
